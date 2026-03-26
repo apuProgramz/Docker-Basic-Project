3eb60bdb945baee14d765500f4e689adb73d8b73
Apurva Ghare <apughare6@gmail.com>
2026-03-26 17:19:46 +0530

    final compose

--- backend/app.py
+++ backend/app.py
@@ -50,4 +50,4 @@ def view():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=8000, use_reloader=False)
